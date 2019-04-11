39180dbbd54af3cba384634b5d245f61a880943d
Luke <lukebulosan@gmail.com>
2019-04-10 19:35:52 -1000

    Add Footer and NavBar

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
+import { Container, Icon, Grid, Button } from 'semantic-ui-react';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
-    const divStyle = { paddingTop: '15px' };
     return (
-        <footer>
-          <div style={divStyle} className="ui center aligned container">
-            <hr />
-              Department of Information and Computer Sciences <br />
-              University of Hawaii<br />
-              Honolulu, HI 96822
-          </div>
-        </footer>
+        <div fluid className="footer-background">
+          <Grid container columns={2}>
+            <Grid.Column>About Akamai Nook
+              <hr/>
+              <p>Akamai Nook is a team of college students who want
+                to provide a simple and efficient way to find and share
+                study spots on Oahu.
+              </p>
+            </Grid.Column>
+            <Grid.Column>Links
+              <hr/>
+              <Button icon>Our Github Page
+                <Icon huge="true" name='github square'/>
+              </Button>
+            </Grid.Column>
+          </Grid>
+        </div>
     );
   }
 }
