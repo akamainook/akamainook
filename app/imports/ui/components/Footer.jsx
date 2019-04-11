04c7a1ecc62a43737fe67b7477240d284c1d029d
Luke <lukebulosan@gmail.com>
2019-04-10 22:03:55 -1000

    Update footer desc

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Icon, Grid, Button } from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
+import { Menu } from 'semantic-ui-react/dist/commonjs/collections/Menu';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
@@ -9,14 +11,16 @@ class Footer extends React.Component {
           <Grid container columns={2}>
             <Grid.Column>About Akamai Nook
               <hr/>
-              <p>Akamai Nook is a team of college students who want
+              <p>Akamai Nook is created by a team of college students who want
                 to provide a simple and efficient way to find and share
                 study spots on Oahu.
               </p>
             </Grid.Column>
             <Grid.Column>Links
               <hr/>
-              <Button icon>Our Github Page
+              <Button   as='a'
+                        href='https://github.com/akamainook' icon>
+                Our Github Page
                 <Icon huge="true" name='github square'/>
               </Button>
             </Grid.Column>
