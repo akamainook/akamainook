6a4bb5ca5bfaf732cd2950f8352b8e5a7a93c902
Luke <lukebulosan@gmail.com>
2019-04-22 19:22:25 -1000

    wip

--- app/imports/ui/pages/ListNook.jsx
+++ app/imports/ui/pages/ListNook.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Container, Table, Header, Loader } from 'semantic-ui-react';
-import { Stuffs } from '/imports/api/stuff/stuff';
-import StuffItem from '/imports/ui/components/StuffItem';
+import { Container, Header, Loader, Card } from 'semantic-ui-react';
+import { Nooks } from '/imports/api/nook/nook';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
+import Nook from '/imports/ui/components/Nook';
 
-/** Renders a table containing all of the Nook documents. Use <NookItem> to render each row. */
-class ListStuff extends React.Component {
+/** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
+class ListNooks extends React.Component {
 
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
@@ -18,37 +18,27 @@ class ListStuff extends React.Component {
   renderPage() {
     return (
         <Container>
-          <Header as="h2" textAlign="center">List Stuff</Header>
-          <Table celled>
-            <Table.Header>
-              <Table.Row>
-                <Table.HeaderCell>Name</Table.HeaderCell>
-                <Table.HeaderCell>Quantity</Table.HeaderCell>
-                <Table.HeaderCell>Condition</Table.HeaderCell>
-                <Table.HeaderCell>Edit</Table.HeaderCell>
-              </Table.Row>
-            </Table.Header>
-            <Table.Body>
-              {this.props.stuffs.map((stuff) => <StuffItem key={stuff._id} stuff={stuff} />)}
-            </Table.Body>
-          </Table>
+          <Header inverted as="h2" textAlign="center">List Nooks</Header>
+          <Card.Group>
+            {this.props.nooks.map((nook, index) => <Nook key={index} nook={nook}/>)}
+          </Card.Group>
         </Container>
     );
   }
 }
 
 /** Require an array of Stuff documents in the props. */
-ListStuff.propTypes = {
-  stuffs: PropTypes.array.isRequired,
+ListNooks.propTypes = {
+  nooks: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
 };
 
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
 export default withTracker(() => {
   // Get access to Stuff documents.
-  const subscription = Meteor.subscribe('Stuff');
+  const subscription = Meteor.subscribe('Nooks');
   return {
-    stuffs: Stuffs.find({}).fetch(),
-    ready: subscription.ready(),
+    nooks: Nooks.find({}).fetch(),
+    ready: (subscription.ready()),
   };
-})(ListStuff);
+})(ListNooks);
